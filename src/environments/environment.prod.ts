b8f7547d98805591becd28bd4da5fe01b35fbbeb
Erick Santizo <esantizo@octagt.com>
2020-04-08 14:00:49 -0600

    deploy to http api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://31m1uop4pi.execute-api.us-east-1.amazonaws.com/prod',
+  apiUrl: 'https://api.cashier2go.com', //'https://31m1uop4pi.execute-api.us-east-1.amazonaws.com/prod',
   bucket: 'https://octagt-erp.s3.amazonaws.com/'
   // apiLocUrl: '7ea1cff846ab05c3a53b2471383af0f1'
 };
